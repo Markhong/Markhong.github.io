5f3e6707192782357965c70db578cf8afa0a87d6
Markhong <hongqiongmiao@163.com>
2020-01-12 20:45:55 +0800

    first commit

---
+++ precache-manifest.d3d05c0e2928fe70517794be6ea20e33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71b7dfaa00e3c72ac6ad42e9a1d0ca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "257f4c382855cc279522",
+    "url": "/static/css/main.9cda0170.chunk.css"
+  },
+  {
+    "revision": "9b505f65499d4a0501ba",
+    "url": "/static/js/2.d697ad8f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.d697ad8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "257f4c382855cc279522",
+    "url": "/static/js/main.bb8d1d9b.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "bdf05f515678c9befe77e0c442523a24",
+    "url": "/static/media/Rufina-Bold.bdf05f51.ttf"
+  },
+  {
+    "revision": "010f8d276ba6a095e72f299c3136122b",
+    "url": "/static/media/kuma.010f8d27.svg"
+  },
+  {
+    "revision": "2c1d34064615edc72b5af4a96ffaef4e",
+    "url": "/static/media/kuma.2c1d3406.ttf"
+  },
+  {
+    "revision": "6ea26f95e687a547e86d262dddc8ea99",
+    "url": "/static/media/kuma.6ea26f95.eot"
+  },
+  {
+    "revision": "867f357a7acca795017797c4c7e9da08",
+    "url": "/static/media/kuma.867f357a.woff"
+  }
+]);
